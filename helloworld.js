e9a85c61df16eaa3f09d2ee45b3171b5c81e8784
Ctbenoit60 <ctbenoit60@gmail.com>
2023-09-25 15:19:51 -0500

    adding the index file and the functions for exercise on slide 84

---
+++ helloworld.js
@@ -0,0 +1,23 @@
+function Sum(a, b){
+        console.log (a+b)
+    }
+    
+    Sum('butter', 'fly')
+
+function Sum2(a, b){
+    console.log (a-b)
+}
+
+Sum2(567, 89)
+
+function Sum3(a, b){
+    console.log (a*b)
+}
+
+Sum3('4', '6.3')
+
+function Sum4(a, b){
+    console.log (a/b)
+}
+
+Sum4(567, 89)
